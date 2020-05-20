76eb5469e4f676e8ec3ca03c8f203f3a1626d844
mariosknl <mariosknl@outlook.com>
2020-05-20 12:54:45 +0300

    Controller actions added && root_path was set

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,24 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    @users = User.all
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name)
+  end
+end
