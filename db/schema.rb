fc6f8e2413b01301f9f74df6dc8ab075e821d8c8
mariosknl <mariosknl@outlook.com>
2020-05-20 15:07:44 +0300

    Migrate event model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_20_091142) do
+ActiveRecord::Schema.define(version: 2020_05_20_105144) do
+
+  create_table "events", force: :cascade do |t|
+    t.datetime "date"
+    t.string "location"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
