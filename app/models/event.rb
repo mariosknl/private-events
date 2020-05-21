7d6c9eb6eb90ae9b82a4659aab13c4ae4e5b6c57
Kubilay <kblycaglayan@gmail.com>
2020-05-21 10:46:58 +0300

    Associations between models simplified

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ApplicationRecord
   has_many :invitations, foreign_key: 'attended_event_id'
-  has_many :attendees, class_name: 'User', through: :invitations, foreign_key: 'attended_event_id'
-  belongs_to :user, foreign_key: 'creator_id'
+  has_many :attendees, through: :invitations
+  belongs_to :creator, class_name: 'User', foreign_key: 'creator_id'
 end
