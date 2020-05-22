204827b8231675a11bd05e127febd85954e8fa88
mariosknl <mariosknl@outlook.com>
2020-05-22 16:32:16 +0300

    Integration Tests added for Events Management

--- spec/features/events_management_spec.rb
+++ spec/features/events_management_spec.rb
@@ -3,16 +3,33 @@ require 'rails_helper'
 RSpec.feature 'EventsManagements', type: :feature do
   before :each do
     User.create(name: 'Test User')
+    User.create(name: 'Test User2')
     visit '/sessions/new'
     within('#new_user') do
       fill_in 'user_name', with: 'Test User'
     end
     click_button 'Sign In'
-    visit ''
-    # Event.create(description: '')
   end
 
   it 'allows user to create an event' do
-    expect(page).to have_current_path(root_path)
+    click_link 'Create New Event'
+    within '#new_event' do
+      fill_in 'event_description', with: 'Test Event'
+      fill_in 'event_date', with: '2020-10-10'
+      fill_in 'event_location', with: 'Ankara'
+    end
+    click_button 'Create'
+    expect(Event.nil?).to be false
+  end
+
+  it 'allows users to invite users that are not been invited' do
+    click_link 'Create New Event'
+    within '#new_event' do
+      fill_in 'event_description', with: 'Test Event'
+      fill_in 'event_date', with: '2020-10-10'
+      fill_in 'event_location', with: 'Ankara'
+    end
+    click_button 'Create'
+    expect(page).to have_content('Invite')
   end
 end
