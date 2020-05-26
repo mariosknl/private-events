5bdee49ac354bbfc510cb509483eb2fd677eb0e5
mariosknl <mariosknl@outlook.com>
2020-05-26 11:09:58 +0300

    Uniqueness added on the model level

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -1,4 +1,6 @@
 class Invitation < ApplicationRecord
   belongs_to :attendee, class_name: 'User'
   belongs_to :attended_event, class_name: 'Event'
+  validates :attendee_id, uniqueness: { scope: :attended_event_id,
+                                        message: 'can created only one invitation for person for party' }
 end
